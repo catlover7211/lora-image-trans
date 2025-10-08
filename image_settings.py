6f0affd27ae6f27ec7605c322744184ab5b98050
catlover <bm963852@gmail.com>
2025-10-08 20:29:27 +0800

    0.4.13

--- image_settings.py
+++ image_settings.py
@@ -38,7 +38,7 @@ class ImageSettings:
     color_mode: ColorMode = "gray"
     """預設以灰階做運動檢測，再在編碼前轉回 BGR，兼顧壓縮與相容性。"""
 
-    codec: VideoCodec = 'h264'
+    codec: VideoCodec = 'jpeg'
     """影像編碼器類型。預設改用 H.264，因為在 160×120@120kbps 下壓縮效率最佳。"""
 
     wavelet_levels: int = 1
