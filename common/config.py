9f5796873520f304aa79f22a53f92f8c4abef2e2
catlover <bm963852@gmail.com>
2025-11-06 20:20:04 +0800

    1.0.43

--- common/config.py
+++ common/config.py
@@ -16,8 +16,8 @@ DEFAULT_HEIGHT = 9*10
 DEFAULT_JPEG_QUALITY = 85
 
 # Compressed Sensing settings
-CS_MEASUREMENT_RATE = 0.02  # 2% sampling rate
-CS_BLOCK_SIZE = 64  # 64x64 pixel blocks
+CS_MEASUREMENT_RATE = 0.01  # 1% sampling rate
+CS_BLOCK_SIZE = 32  # 32x32 pixel blocks
 
 # Buffer settings
 MAX_FRAME_SIZE = 65535  # Maximum 2^16-1 bytes per frame
