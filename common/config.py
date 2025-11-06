3d0fa2c34bbaf76efeb5615a12f259533d1f53bf
catlover <bm963852@gmail.com>
2025-11-06 20:59:14 +0800

    1.0.51

--- common/config.py
+++ common/config.py
@@ -20,7 +20,7 @@ CS_MEASUREMENT_RATE = 0.01  # 1% sampling rate
 CS_BLOCK_SIZE = 16  # 16x16 pixel blocks
 
 # Buffer settings
-MAX_FRAME_SIZE = 65535  # Maximum 2^16-1 bytes per frame
+MAX_FRAME_SIZE = 700000  # Maximum 2^16-1 bytes per frame
 CHUNK_SIZE = 5000  # Bytes per chunk for LoRa transmission
 
 # Flow control settings
